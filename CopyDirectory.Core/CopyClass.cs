7510f067491bff206ace2cd325f938e340ea3247
Elaine <elaineakemi@gmail.com>
2018-02-15 21:10:37 +0000

    added project

---
+++ CopyDirectory.Core/CopyClass.cs
@@ -0,0 +1,45 @@
+﻿///  20180206 Technical Coding Exercise C# Algorithm
+using System;
+using System.IO;
+
+namespace CopyDirectory.Core
+{
+    /// <summary>
+    /// Class to be independent of the UI
+    /// </summary>
+    public class CopyClass
+    {
+        public void StartCopy(string source, string target)
+        {
+            try
+            {
+                //Create directory in case not exist - for sub-folders
+                Directory.CreateDirectory(target);
+                foreach (var file in Directory.GetFiles(source))
+                {
+                    //display file is being copied
+                    Console.WriteLine("Copying file " + Path.GetFileName(file));
+
+                    //this will overwrite all the files it already exist. To not overwrite must set flag to false
+                    bool flagOverwrite = true;
+                    File.Copy(file, Path.Combine(target, Path.GetFileName(file)), flagOverwrite);
+
+                    //display that file had finished copy
+                    Console.WriteLine(Path.GetFileName(file) + " file copied.\n");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            foreach (var directory in Directory.GetDirectories(source))
+            {
+                //Recursive call to method
+                StartCopy(directory, Path.Combine(target, Path.GetFileName(directory)));
+            }
+        }
+
+
+    }
+}
---
+++ CopyDirectory/Program.cs
@@ -0,0 +1,47 @@
+﻿///  20180206 Technical Coding Exercise C# Algorithm
+using System;
+
+namespace CopyDirectory
+{
+    /// <summary>
+    /// Main Program.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathSource = null;
+            string pathTarget = null;
+
+            //only accept valid Source
+            while(pathSource == null)
+            {
+                Console.WriteLine("Please type in the SOURCE path: (E.g. C:\\Users\\Elaine\\OldDocuments)");
+                pathSource = Console.ReadLine();
+                if (!System.IO.Directory.Exists(pathSource))
+                {
+                    Console.WriteLine("The specified path does not exist.");
+                    pathSource = null;
+                }
+            }
+
+            //only accept valid Target
+            while(pathTarget==null)
+            {
+                Console.WriteLine("Please type in the TARGET path: (E.g. C:\\Users\\Elaine\\NewDocuments)");
+                pathTarget = Console.ReadLine();
+                if (!System.IO.Directory.Exists(pathTarget))
+                {
+                    Console.WriteLine("The specified path does not exist.");
+                    pathTarget = null;
+                }
+            }
+
+            Core.CopyClass objCopy = new Core.CopyClass();
+            objCopy.StartCopy(pathSource, pathTarget);
+
+            //Wait to close the application
+            Console.Read();
+        }
+    }
+}
